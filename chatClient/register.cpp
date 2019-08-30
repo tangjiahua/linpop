90e586d6a7b5ab35fd81df8d906ca6daaf7bac43
tangjiahua <2035189076@qq.com>
2019-08-30 15:24:17 +0800

    add function TalkBoxtoShow

--- chatClient/register.cpp
+++ chatClient/register.cpp
@@ -42,6 +42,23 @@ void Register::commitTo()
         return;
     }
 
+    strcat(registInfoSendToServer,"0|");
+    strcat(registInfoSendToServer,rf.name);
+    strcat(registInfoSendToServer,"|");
+    strcat(registInfoSendToServer,rf.pwd);
+    qDebug() << registInfoSendToServer << endl;
+
+    if(!strcmp(rf.name,""))
+    {
+        ui->label_new_account->setText(QString("username is empty, please fill in username"));
+        return;
+    }
+    else if(!strcmp(rf.pwd,"")||!strcmp(cfmpwd,""))
+    {
+        ui->label_new_account->setText(QString("please set password"));
+        return;
+    }
+
     int rgfd = cs.connectTo(serverIp,serverPort);
 
     if(rgfd == -1){
@@ -49,57 +66,40 @@ void Register::commitTo()
         return;
     }
 
-    strcat(registInfoSendToServer,"0|");
-    strcat(registInfoSendToServer,rf.name);
-    strcat(registInfoSendToServer,"|");
-    strcat(registInfoSendToServer,rf.pwd);
-    qDebug() << registInfoSendToServer << endl;
-
-    if(!strcmp(rf.name,"") || !strcmp(rf.pwd,""))
-    {
-        qDebug() << "pwd or id is empty!" << endl;
-        return;
-    }
-
     send(rgfd,&registInfoSendToServer,sizeof(registInfoSendToServer),0);
 
-
-    char is_registered = '-1';//status of the account if it has been registered before?
-
+    char is_registered = '1';//status of the account if it has been registered before?
 
     recv(rgfd,&is_registered,sizeof(char),0);
 
-
-
     if(is_registered == '1'){
         QMessageBox msg(this);//对话框设置父组件
-        msg.setWindowTitle("congratrulations");//对话框标题
-        msg.setText("registed successfully, you can go to login now!");//对话框提示文本
+        msg.setWindowTitle("registed successfully");//对话框标题
+        msg.setText("you can go to login now!");//对话框提示文本
         msg.setIcon(QMessageBox::Information);//设置图标类型
         msg.setStandardButtons(QMessageBox::Ok);//对话框上包含的按钮
 
         if(msg.exec() == QMessageBox::Ok)//模态调用
         {
+            qDebug() << "regist succeeded";//数据处理
             this->close();
-           qDebug() << "regist succeeded";//数据处理
         }
 //        ui->label_new_account->setText(QString("registed successfully, you can go to login now!"));
-        qDebug()<<"you can go to login now"<<endl;
+//        qDebug()<<"you can go to login now"<<endl;
     }
-    else if(is_registered == '0'){
+    else{
         QMessageBox msg(this);//对话框设置父组件
-        msg.setWindowTitle("information");//对话框标题
-        msg.setText("accout already been registered before by someone");//对话框提示文本
+        msg.setWindowTitle("register failed");//对话框标题
+        msg.setText("this username has been registered");//对话框提示文本
         msg.setIcon(QMessageBox::Information);//设置图标类型
         msg.setStandardButtons(QMessageBox::Ok);//对话框上包含的按钮
 
         if(msg.exec() == QMessageBox::Ok)//模态调用
         {
-           qDebug() << "regiset failed";//数据处理
+           qDebug() << "regist failed";//数据处理
            return;
         }
     }
-
 }
 
 
